dbe6245220d85aa068b0606ac0c4d45981219492
Wouter van Rooy <wouter.van.rooy@axon.tv>
2014-11-20 11:26:34 +0100

    Add GPL license info

--- gc-export.user.js
+++ gc-export.user.js
@@ -1,3 +1,20 @@
+/*
+ * This file is part of GC-Export.
+ * 
+ * GC-Export is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // ==UserScript==
 // @name        GreenChoice meter records export script
 // @namespace   https://dev.nullpointer.nl/energy/gc-export
@@ -8,7 +25,7 @@
 // @match       https://dossier.greenchoice.nl/mijn-dossier/mijn-verbruik.aspx*
 // @copyright   2014, Wouter van Rooy
 // ==/UserScript==
-
+ 
 const ELEC_TABLE_ID = 'ctl00_ctl00_Content_ContentRightPlaceholder_MeterstandenUserControl_GeordendStandenOverzichtStroom';
 const GAS_TABLE_ID = 'ctl00_ctl00_Content_ContentRightPlaceholder_MeterstandenUserControl_GeordendStandenoverzichtGas';
 
