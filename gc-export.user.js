9b1b4c5cb605ef646bd42d5abe3cfba6c0e61c7a
Wouter van Rooy <info@woutervanrooy.nl>
2015-10-21 20:19:55 +0200

    Bump version number

--- gc-export.user.js
+++ gc-export.user.js
@@ -19,7 +19,7 @@
 // @name        GreenChoice meter records export script
 // @namespace   https://dev.nullpointer.nl/energy/gc-export
 // @author      Wouter van Rooy
-// @version     1.1.1
+// @version     1.1.2
 // @description This user script helps you export the meter records from your personal GreenChoice file.
 // @match       http://dossier.greenchoice.nl/Mijn-Dossier/Mijn-verbruik.aspx*
 // @match       http://dossier.greenchoice.nl/mijn-dossier/mijn-verbruik.aspx*
